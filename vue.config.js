3a722bfbf9bf6437d8d02c51491bcc26953d0e44
James O'Shea <jamesoshea89@hotmail.com>
2021-06-12 20:18:35 +0200

    change theme and icon in prod app

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pwa: {
+    manifestOptions: {
+      themecolor: "#FFFFFF",
+      icons: [
+        {
+          src: "/public/favicon.png",
+          sizes: "32x32",
+          type: "image/png"
+        }
+      ]
+    }
+  }
+};
